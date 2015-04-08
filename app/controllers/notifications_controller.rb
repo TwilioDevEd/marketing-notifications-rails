45b7f9915b78a569be89398d5b92d55909bc1d01
Jarod Reyes <jarodreyes@gmail.com>
2015-04-07 17:24:08 -0700

    Updated the rails version to mimick node implementation.

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,28 +1,21 @@
 class NotificationsController < ApplicationController
-  before_action :set_subscriber, only: [:incoming]
-  
+
   # Receive incoming SMS
   def incoming
+    # Grab the phone number from incoming Twilio params
+    @phone_number = params[:From]
+
+    # Find the subscriber associated with this number or create a new one
+    @new_subscriber = Subscriber.exists?(:phone_number => @phone_number) === false
+    @subscriber = Subscriber.first_or_create(:phone_number => @phone_number)
+
+    @body = if params[:Body].nil? then '' else params[:Body].downcase end
     begin
       if @new_subscriber
         output = "Thanks for contacting TWBC! Text 'subscribe' if you would to receive updates via text message."
       else
-        if @body == 'subscribe' || @body == 'unsubscribe'
-          # If the user has subscribed flip the bit
-          # and let them know
-          subscribed = @body === 'subscribe'
-          @subscriber.update :subscribed => subscribed
-
-          # Respond appropriately
-          output = "You are now subscribed for updates."
-          if !@subscriber.subscribed
-            output = "You have unsubscribed from notifications. Test 'subscribe' to start receieving updates again"
-          end
-        else
-          # If we don't recognize the command, text back with the list of
-          # available commands
-          output = "Sorry, we don't recognize that command. Available commands are: 'subscribe' or 'unsubscribe'."
-        end
+        # Process the command from our Subscriber
+        output = process_message(@body, @subscriber)
       end
     rescue
       output = "Something went wrong. Try again."
@@ -35,7 +28,6 @@ class NotificationsController < ApplicationController
   # Create a new notification
   def new
     message = params[:message]
-    puts message
     image_url = params[:image_url]
 
     Subscriber.all.each do |s|
@@ -50,21 +42,30 @@ class NotificationsController < ApplicationController
   end
 
   def index
-
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    # See above ---> before_action :set_subscriber, only: [:receiver]
-    def set_subscriber
-      # Grab the phone number from incoming Twilio params
-      @phone_number = params[:From]
 
-      # Find the subscriber associated with this number or create a new one
-      @new_subscriber = Subscriber.exists?(:phone_number => @phone_number) === false
-      @subscriber = Subscriber.first_or_create(:phone_number => @phone_number)
+    # Process incoming SMS
+    def process_message(message, subscriber)
+      if message == 'subscribe' || message == 'unsubscribe'
+        # If the user has subscribed flip the bit
+        # and let them know
+        subscribed = message === 'subscribe'
+        subscriber.update :subscribed => subscribed
 
-      @body = if params[:Body].nil? then '' else params[:Body].downcase end
+        # Respond appropriately
+        output = "You are now subscribed for updates."
+        if !subscriber.subscribed
+          output = "You have unsubscribed from notifications. Test 'subscribe' to start receieving updates again"
+        end
+      else
+        # If we don't recognize the command, text back with the list of
+        # available commands
+        output = "Sorry, we don't recognize that command. Available commands are: 'subscribe' or 'unsubscribe'."
+      end
+      return output
     end
 
     # Send an SMS back to the Subscriber
