ec18acddbd01a54f1b9f6c3dff1a149906eaed40
Agustin Camino <agustin.camino@gmail.com>
2015-12-04 09:31:59 -0500

    Fix incoming method on notifications controller
    * Add test suite

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,21 +1,20 @@
 class NotificationsController < ApplicationController
-
   # Receive incoming SMS
   def incoming
     # Grab the phone number from incoming Twilio params
-    @phone_number = params[:From]
+    phone_number = params[:From]
 
     # Find the subscriber associated with this number or create a new one
-    @new_subscriber = Subscriber.exists?(:phone_number => @phone_number) === false
-    @subscriber = Subscriber.first_or_create(:phone_number => @phone_number)
+    subscriber_exists = Subscriber.exists?(phone_number: phone_number)
+    subscriber = Subscriber.find_or_create_by(phone_number: phone_number)
 
-    @body = if params[:Body].nil? then '' else params[:Body].downcase end
     begin
-      if @new_subscriber
-        output = "Thanks for contacting TWBC! Text 'subscribe' if you would to receive updates via text message."
+      if subscriber_exists
+        # Process the command from our subscriber
+        body = params.fetch(:Body, '').downcase
+        output = process_message(body, subscriber)
       else
-        # Process the command from our Subscriber
-        output = process_message(@body, @subscriber)
+        output = "Thanks for contacting TWBC! Text 'subscribe' if you would to receive updates via text message."
       end
     rescue
       output = "Something went wrong. Try again."
@@ -45,35 +44,34 @@ class NotificationsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+  # Use callbacks to share common setup or constraints between actions.
 
-    # Process incoming SMS
-    def process_message(message, subscriber)
-      if message == 'subscribe' || message == 'unsubscribe'
-        # If the user has subscribed flip the bit
-        # and let them know
-        subscribed = message === 'subscribe'
-        subscriber.update :subscribed => subscribed
+  # Process incoming SMS
+  def process_message(message, subscriber)
+    if message == 'subscribe' || message == 'unsubscribe'
+      # If the user has subscribed flip the bit
+      # and let them know
+      subscribed = message == 'subscribe'
+      subscriber.update subscribed: subscribed
 
-        # Respond appropriately
-        output = "You are now subscribed for updates."
-        if !subscriber.subscribed
-          output = "You have unsubscribed from notifications. Test 'subscribe' to start receieving updates again"
-        end
-      else
-        # If we don't recognize the command, text back with the list of
-        # available commands
-        output = "Sorry, we don't recognize that command. Available commands are: 'subscribe' or 'unsubscribe'."
+      # Respond appropriately
+      output = "You are now subscribed for updates."
+      if !subscriber.subscribed
+        output = "You have unsubscribed from notifications. Test 'subscribe' to start receieving updates again"
       end
-      return output
+    else
+      # If we don't recognize the command, text back with the list of
+      # available commands
+      output = "Sorry, we don't recognize that command. Available commands are: 'subscribe' or 'unsubscribe'."
     end
+    return output
+  end
 
-    # Send an SMS back to the Subscriber
-    def respond(message)
-      response = Twilio::TwiML::Response.new do |r|
-        r.Message message
-      end
-      render text: response.text
+  # Send an SMS back to the Subscriber
+  def respond(message)
+    response = Twilio::TwiML::Response.new do |r|
+      r.Message message
     end
-
+    render text: response.text
+  end
 end
