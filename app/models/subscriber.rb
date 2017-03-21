80fc865dc7ae5c0966e21067f4c7a7dde4f388cd
Paul Kamp <pkamp3@users.noreply.github.com>
2017-03-21 16:12:54 -0700

    Remove references  to other tutorials

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,6 +1,6 @@
 class Subscriber < ActiveRecord::Base
 
-  # Notify our appointment attendee X minutes before the appointment time
+  # Create an opt-in subscriber entry for marketing notifications
   def send_message(msg, image_url)
     @twilio_number = ENV['TWILIO_NUMBER']
     @client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
